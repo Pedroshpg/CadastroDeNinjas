ab83a9b08ac2825cf2b6f7eb946962815e849e02
Pedro Henrique dos Santos Noleto <pedronoleto2018@hotmail.com>
2025-06-06 20:39:09 -0300

    Adicionado a dependencia h2 para testes em BD local

--- src/main/java/dev/java10x/CadastrodeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastrodeNinjas/Missoes/MissoesModel.java
@@ -2,12 +2,19 @@ package dev.java10x.CadastrodeNinjas.Missoes;
 
 import dev.java10x.CadastrodeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_Missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/dev/java10x/CadastrodeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastrodeNinjas/Ninjas/NinjaModel.java
@@ -2,13 +2,18 @@ package dev.java10x.CadastrodeNinjas.Ninjas;
 
 import dev.java10x.CadastrodeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 // entity transforma uma classe em uma entidade do BD
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,36 +27,4 @@ public class NinjaModel {
     private MissoesModel missoes;
 
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
